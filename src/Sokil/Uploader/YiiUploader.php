e4e354542087297f89a2202bef129135cb4e17d3
Dmytro Sokil <dmytro.sokil@gmail.com>
2014-01-06 23:42:16 +0200

    implement uploader for Yii framework

---
+++ src/Sokil/Uploader/YiiUploader.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Sokil\Uploader;
+
+class YiiUploader extends Uploader implements IApplicationComponent
+{
+    private $_initialized = false;
+    
+    /**
+     * Initializes the application component.
+     * This method is invoked after the application completes configuration.
+     */
+    public function init()
+    {
+        $this->_initialized = true;
+    }
+    
+    /**
+     * @return boolean whether the {@link init()} method has been invoked.
+     */
+    public function getIsInitialized()
+    {
+        return $this->_initialized;
+    }
+
+    /**
+     * Register javascript and css files
+     * 
+     * @return \Sokil\Uploader\YiiUploader
+     */
+    public function registerScripts()
+    {
+        $path = Yii::app()->getAssetManager()
+            ->publish(__DIR__ . '/../js/');
+        
+        Yii::app()->getClientScript()
+            ->registerScriptFile($path . '/uploader.js');
+        
+        return $this;
+    }
+}
